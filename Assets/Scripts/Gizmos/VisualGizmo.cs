66605235f3ff210634d0acfa59c085c756a7fb99
Osaze Shears <yoze15@gmail.com>
2024-09-19 20:54:46 -0400

    Added visual gizmo

---
+++ Assets/Scripts/Gizmos/VisualGizmo.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace OSGames.Utilities.Gizmos{
+
+    public class VisualGizmo : MonoBehaviour
+    {
+        [SerializeField] public Color m_GizmoColor;
+        [SerializeField] public Vector3 m_GizmoSize;
+        [SerializeField] public Vector3 m_GizmosShapeOffset;
+        [SerializeField] public string m_GizmoText;
+        [SerializeField] public Vector3 m_GizmoTextOffset;
+        [SerializeField] public GizmoShape m_GizmoShape;
+        [SerializeField] public bool m_ShowGizmo;
+        [SerializeField] public BoxCollider boxCollider;
+        [SerializeField] public SphereCollider sphereCollider;
+        [SerializeField] public Direction m_ShowDirection;
+        [SerializeField] public float m_DirectionLength = 1f;
+        [SerializeField] public Color m_DirectionColor = Color.yellow;
+
+        public enum GizmoShape
+        {
+            Cube,
+            WireCube,
+            Sphere,
+            WireSphere
+        }
+
+        public enum Direction
+        {
+            None,
+            Right,
+            Left,
+            Up,
+            Down,
+            Forward,
+            Back
+        }
+
+        #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            if (!m_ShowGizmo) return;
+
+            // Show base gizmo
+            Gizmos.color = m_GizmoColor;
+            if (m_GizmoShape == GizmoShape.Cube)
+            {
+                Gizmos.DrawCube(transform.position + m_GizmosShapeOffset, m_GizmoSize);
+                if (boxCollider) Gizmos.DrawCube(transform.position + boxCollider.center, boxCollider.size);
+            }
+            if (m_GizmoShape == GizmoShape.WireCube)
+            {
+                Gizmos.DrawWireCube(transform.position + m_GizmosShapeOffset, m_GizmoSize);
+                if (boxCollider) Gizmos.DrawWireCube(transform.position + boxCollider.center, boxCollider.size);
+            }
+            if (m_GizmoShape == GizmoShape.Sphere)
+            {
+                Gizmos.DrawSphere(transform.position + m_GizmosShapeOffset, m_GizmoSize.x);
+                if (sphereCollider) Gizmos.DrawSphere(transform.position + sphereCollider.center, sphereCollider.radius);
+            }
+            if (m_GizmoShape == GizmoShape.WireSphere)
+            {
+                Gizmos.DrawWireSphere(transform.position + m_GizmosShapeOffset, m_GizmoSize.x);
+                if (sphereCollider) Gizmos.DrawWireSphere(transform.position + sphereCollider.center, sphereCollider.radius);
+            }
+
+            // Show direction
+            DrawDirectionGizmo();
+
+            // Show text
+            Handles.Label(transform.position + m_GizmoTextOffset, m_GizmoText);
+        }
+
+        private void DrawDirectionGizmo()
+        {
+            if (m_ShowDirection == Direction.None) return;
+
+            Vector3 startPoint = transform.position;
+            Vector3 endPoint = startPoint;
+
+            switch (m_ShowDirection)
+            {
+                case Direction.Right:
+                    endPoint += transform.right * m_DirectionLength;
+                    break;
+                case Direction.Left:
+                    endPoint -= transform.right * m_DirectionLength;
+                    break;
+                case Direction.Up:
+                    endPoint += transform.up * m_DirectionLength;
+                    break;
+                case Direction.Down:
+                    endPoint -= transform.up * m_DirectionLength;
+                    break;
+                case Direction.Forward:
+                    endPoint += transform.forward * m_DirectionLength;
+                    break;
+                case Direction.Back:
+                    endPoint -= transform.forward * m_DirectionLength;
+                    break;
+            }
+
+            Gizmos.color = m_DirectionColor;
+            Gizmos.DrawLine(startPoint, endPoint);
+            Gizmos.DrawSphere(endPoint, 0.1f * m_DirectionLength);
+        }
+        #endif
+    }
+}
