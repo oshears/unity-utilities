09d76763c012c0f11395a062b3770de1ea1b61de
Osaze Shears <yoze15@gmail.com>
2024-09-30 20:24:46 -0400

    Added list shuffling

---
+++ Assets/Scripts/Utilities/Collections/ShuffleList.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+
+namespace OSGames.Utilities{
+
+    public static class ListFunctions {
+
+        // private static Random rng = new Random();  
+
+        public static void ShuffleList<T>(this IList<T> list)  
+        {  
+            uint seed = (uint) DateTime.Now.Millisecond;
+            if (seed == 0) seed = 1;
+            Unity.Mathematics.Random rng = new Unity.Mathematics.Random(seed);
+            int n = list.Count;  
+            while (n > 1) {  
+                n--;  
+                int k = rng.NextInt(n + 1);  
+                T value = list[k];  
+                list[k] = list[n];  
+                list[n] = value;  
+            }  
+        }
+    }
+
+}
