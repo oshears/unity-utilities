7b9cbd2b93d3c7d32f611f24e18b07ecb8d87fbc
Osaze Shears <yoze15@gmail.com>
2024-10-19 15:58:21 -0400

    Added billborad component

---
+++ Assets/Scripts/Utilities/Transform/Billboard.cs
@@ -0,0 +1,17 @@
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+namespace OSGames.Utilities{
+    
+    public class Billboard : MonoBehaviour {
+
+        [Tooltip("The additional offset angle to adjust the billboard effect.")]
+        [SerializeField] Vector3 m_AngleOffset;
+
+        void Update(){
+            transform.LookAt(Camera.main.transform.position, -Vector3.up);
+            transform.eulerAngles += m_AngleOffset;
+        }
+        
+    }
+}
